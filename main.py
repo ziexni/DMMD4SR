0df38adc3992699bd4598251dbe99544da8256b8
ziexni <168633568+ziexni@users.noreply.github.com>
2026-04-13 13:28:51 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,151 @@
+import os
+import numpy as np
+import torch
+import argparse
+
+from torch.utils.data import DataLoader, RandomSampler, SequentialSampler
+
+from datasets import DMMD4SRDataset
+
+from trainers import DMMD4SRTrainer
+from models import DMMD4SRModel
+from utils import EarlyStopping, get_user_seqs, check_path, set_seed
+
+
+def show_args_info(args):
+    print(f"--------------------Configure Info:------------")
+    for arg in vars(args):
+        print(f"{arg:<30} : {getattr(args, arg):>35}")
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    # system args
+    parser.add_argument("--data_dir", default="../process_data/", type=str)
+    parser.add_argument("--output_dir", default="output/", type=str)
+    parser.add_argument("--data_name", default="kuaishou", type=str)
+    parser.add_argument("--do_eval", action="store_true")
+    parser.add_argument("--model_idx", default=0, type=int, help="model idenfier 10, 20, 30...")
+    parser.add_argument("--gpu_id", type=str, default="0", help="gpu_id")
+
+    # model args
+    parser.add_argument("--model_name", default="DMMD4SR", type=str)
+    parser.add_argument("--hidden_size", type=int, default=64, help="hidden size of transformer model")
+    parser.add_argument("--num_hidden_layers", type=int, default=2, help="number of layers")
+    parser.add_argument("--num_attention_heads", default=2, type=int)
+    parser.add_argument("--hidden_act", default="gelu", type=str)
+    parser.add_argument("--attention_probs_dropout_prob", type=float, default=0.5, help="attention dropout p")
+    parser.add_argument("--hidden_dropout_prob", type=float, default=0.5, help="hidden dropout p")
+    parser.add_argument("--initializer_range", type=float, default=0.02)
+    parser.add_argument("--max_seq_length", default=50, type=int)
+    parser.add_argument("--n_clusters", type=int, default=32, help="number of clusters")
+    parser.add_argument("--lambda_history", type=float, default=0.6, help="lambda of history")
+    parser.add_argument("--lambda_intent", type=float, default=0.4, help="lambda of intent")
+
+    # encoder
+    parser.add_argument("--num_experts", type=int, default=4, help="number of num_experts")
+    parser.add_argument("--diff_loss", type=float, default=0.01)
+
+    # multi modal
+    parser.add_argument("--is_use_mm", type=bool, default=True, help="is use mm embedding")
+    parser.add_argument("--is_use_text", type=bool, default=True, help="is use text embedding")
+    parser.add_argument("--is_use_image", type=bool, default=False, help="is use image embedding")
+    parser.add_argument("--text_embedding_path", default='../process_data/text_features.pt', type=str)
+    parser.add_argument("--image_embedding_path", default='../process_data/image_features.pt', type=str)
+    parser.add_argument("--pretrain_emb_dim", type=int, default=512, help="pretrain_emb_dim of clip model")
+
+    # train args
+    parser.add_argument("--lr", type=float, default=0.001, help="learning rate of adam")
+    parser.add_argument("--batch_size", type=int, default=256, help="number of batch_size")
+    parser.add_argument("--epochs", type=int, default=80, help="number of epochs")
+    parser.add_argument("--early_stop_num", type=int, default=10, help="number of early_stop")
+    parser.add_argument("--no_cuda", action="store_true")
+    parser.add_argument("--log_freq", type=int, default=1, help="per epoch print res")
+    parser.add_argument("--seed", default=2024, type=int)
+    
+    # loss weight
+    parser.add_argument("--rec_weight", type=float, default=1.0, help="weight of recommendation loss")
+
+    # learning related
+    parser.add_argument("--weight_decay", type=float, default=0.0, help="weight_decay of adam")
+
+    args = parser.parse_args()
+    set_seed(args.seed)
+    check_path(args.output_dir)
+
+    os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu_id
+    args.cuda_condition = torch.cuda.is_available() and not args.no_cuda
+    print("Using Cuda:", torch.cuda.is_available())
+    
+    args.data_file = args.data_dir + args.data_name + ".txt"
+
+    # Load data
+    user_seq, max_item, valid_rating_matrix, test_rating_matrix = get_user_seqs(args.data_file)
+
+    args.item_size = max_item + 2
+    args.mask_id = max_item + 1
+    
+    # save model args
+    args_str = f"{args.model_name}-{args.data_name}-{args.model_idx}"
+    args.log_file = os.path.join(args.output_dir, args_str + ".txt")
+
+    show_args_info(args)
+
+    with open(args.log_file, "a") as f:
+        f.write(str(args) + "\n")
+
+    # set item score in train set to `0` in validation
+    args.train_matrix = valid_rating_matrix
+
+    # save model
+    checkpoint = args_str + ".pt"
+    args.checkpoint_path = os.path.join(args.output_dir, checkpoint)
+
+    # training data
+    train_dataset = DMMD4SRDataset(args, user_seq, data_type="train")
+    train_sampler = RandomSampler(train_dataset)
+    train_dataloader = DataLoader(train_dataset, sampler=train_sampler, batch_size=args.batch_size)
+
+    eval_dataset = DMMD4SRDataset(args, user_seq, data_type="valid")
+    eval_sampler = SequentialSampler(eval_dataset)
+    eval_dataloader = DataLoader(eval_dataset, sampler=eval_sampler, batch_size=args.batch_size)
+
+    test_dataset = DMMD4SRDataset(args, user_seq, data_type="test")
+    test_sampler = SequentialSampler(test_dataset)
+    test_dataloader = DataLoader(test_dataset, sampler=test_sampler, batch_size=args.batch_size)
+
+    model = DMMD4SRModel(args=args)
+    trainer = DMMD4SRTrainer(model, train_dataloader, eval_dataloader, test_dataloader, args)
+
+    if args.do_eval:
+        trainer.args.train_matrix = test_rating_matrix
+        trainer.load(args.checkpoint_path)
+        print(f"Load model from {args.checkpoint_path} for test!")
+        scores, result_info = trainer.test(0, full_sort=True)
+    else:
+        print(f"Train DMMD4SR")
+        early_stopping = EarlyStopping(args.checkpoint_path, patience=args.early_stop_num, verbose=True)
+        for epoch in range(args.epochs):
+            trainer.train(epoch)
+            # evaluate on NDCG@10, HR@10, MRR
+            scores, _ = trainer.valid(epoch, full_sort=True)
+            early_stopping(np.array(scores[-1:]), trainer.model)
+            if early_stopping.early_stop:
+                print("Early stopping")
+                break
+        
+        trainer.args.train_matrix = test_rating_matrix
+        print("---------------Change to test_rating_matrix!-------------------")
+        # load the best model
+        trainer.model.load_state_dict(torch.load(args.checkpoint_path))
+        scores, result_info = trainer.test(0, full_sort=True)
+
+    print(args_str)
+    print(result_info)
+    with open(args.log_file, "a") as f:
+        f.write(args_str + "\n")
+        f.write(result_info + "\n")
+
+
+if __name__ == '__main__':
+    main()
