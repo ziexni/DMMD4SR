450b71b8e2703a50b8ab068da52a2a4e373d3eda
ziexni <168633568+ziexni@users.noreply.github.com>
2026-04-13 21:22:26 +0900

    Update modules.py

--- modules.py
+++ modules.py
@@ -1,176 +1,74 @@
-import numpy as np
-import copy
-import math
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
+"""
+datamodule.py
+카테고리 제거, 텍스트/이미지 피처만, leave-two-out split
+"""
+
+from torch.utils.data import DataLoader
+from data import get_data
+from datasets import RecDataset
 
 
-def gelu(x):
-    """Implementation of the gelu activation function.
-        For information: OpenAI GPT's gelu is slightly different
-        (and gives slightly different results):
-        0.5 * x * (1 + torch.tanh(math.sqrt(2 / math.pi) *
-        (x + 0.044715 * torch.pow(x, 3))))
-        Also see https://arxiv.org/abs/1606.08415
-    """
-    return x * 0.5 * (1.0 + torch.erf(x / math.sqrt(2.0)))
+INTERACTION_PATH = 'kuaishou_preprocess.pkl'
 
 
-def swish(x):
-    return x * torch.sigmoid(x)
-
-
-ACT2FN = {"gelu": gelu, "relu": F.relu, "swish": swish}
-
-
-class LayerNorm(nn.Module):
-    def __init__(self, hidden_size, eps=1e-12):
-        """Construct a layernorm module in the TF style (epsilon inside the square root).
-        """
-        super(LayerNorm, self).__init__()
-        self.weight = nn.Parameter(torch.ones(hidden_size))
-        self.bias = nn.Parameter(torch.zeros(hidden_size))
-        self.variance_epsilon = eps
-
-    def forward(self, x):
-        u = x.mean(-1, keepdim=True)
-        s = (x - u).pow(2).mean(-1, keepdim=True)
-        x = (x - u) / torch.sqrt(s + self.variance_epsilon)
-        return self.weight * x + self.bias
-
-
-class Embeddings(nn.Module):
-    """Construct the embeddings from item, position.
-    """
-
+class DataModule:
     def __init__(self, args):
-        super(Embeddings, self).__init__()
+        self.args            = args
+        self.max_len         = args.max_len
+        self.neg_sample_size = args.neg_sample_size
+        self.batch_size      = args.batch_size
+        self.num_workers     = args.num_workers
+        self.interaction_path = args.interaction_path
 
-        self.item_embeddings = nn.Embedding(args.item_size, args.hidden_size, padding_idx=0)
-        self.position_embeddings = nn.Embedding(args.max_seq_length, args.hidden_size)
+        self.user_train, self.user_valid, self.user_test, \
+            self.usernum, self.itemnum = get_data(self.interaction_path)
 
-        self.LayerNorm = LayerNorm(args.hidden_size, eps=1e-12)
-        self.dropout = nn.Dropout(args.hidden_dropout_prob)
+        # args에 item_size 주입 (models.py에서 vocab_size로 사용)
+        args.item_size = self.itemnum + 2   # 0: PAD, itemnum+1: MASK
 
-        self.args = args
+    def setup(self):
+        self.train_dataset = RecDataset(
+            self.user_train, self.user_valid, self.user_test,
+            self.itemnum, self.max_len,
+            mode='train',
+        )
+        self.valid_dataset = RecDataset(
+            self.user_train, self.user_valid, self.user_test,
+            self.itemnum, self.max_len,
+            neg_sample_size=self.neg_sample_size,
+            mode='valid',
+        )
+        self.test_dataset = RecDataset(
+            self.user_train, self.user_valid, self.user_test,
+            self.itemnum, self.max_len,
+            neg_sample_size=self.neg_sample_size,
+            mode='test',
+        )
 
-    def forward(self, input_ids):
-        seq_length = input_ids.size(1)
-        position_ids = torch.arange(seq_length, dtype=torch.long, device=input_ids.device)
-        position_ids = position_ids.unsqueeze(0).expand_as(input_ids)
-        items_embeddings = self.item_embeddings(input_ids)
-        position_embeddings = self.position_embeddings(position_ids)
-        embeddings = items_embeddings + position_embeddings
+    def train_dataloader(self):
+        return DataLoader(self.train_dataset,
+                          batch_size=self.batch_size,
+                          shuffle=True,
+                          num_workers=self.num_workers)
 
-        embeddings = self.LayerNorm(embeddings)
-        embeddings = self.dropout(embeddings)
-        return embeddings
+    def val_dataloader(self):
+        return DataLoader(self.valid_dataset,
+                          batch_size=self.batch_size,
+                          shuffle=False,
+                          num_workers=self.num_workers)
 
+    def test_dataloader(self):
+        return DataLoader(self.test_dataset,
+                          batch_size=self.batch_size,
+                          shuffle=False,
+                          num_workers=self.num_workers)
 
-class SelfAttention(nn.Module):
-    def __init__(self, args):
-        super(SelfAttention, self).__init__()
-        if args.hidden_size % args.num_attention_heads != 0:
-            raise ValueError(
-                "The hidden size (%d) is not a multiple of the number of attention "
-                "heads (%d)" % (args.hidden_size, args.num_attention_heads)
-            )
-        self.num_attention_heads = args.num_attention_heads
-        self.attention_head_size = int(args.hidden_size / args.num_attention_heads)
-        self.all_head_size = self.num_attention_heads * self.attention_head_size
-
-        self.query = nn.Linear(args.hidden_size, self.all_head_size)
-        self.key = nn.Linear(args.hidden_size, self.all_head_size)
-        self.value = nn.Linear(args.hidden_size, self.all_head_size)
-
-        self.attn_dropout = nn.Dropout(args.attention_probs_dropout_prob)
-        self.dense = nn.Linear(args.hidden_size, args.hidden_size)
-        self.LayerNorm = LayerNorm(args.hidden_size, eps=1e-12)
-        self.out_dropout = nn.Dropout(args.hidden_dropout_prob)
-
-    def transpose_for_scores(self, x):
-        new_x_shape = x.size()[:-1] + (self.num_attention_heads, self.attention_head_size)
-        x = x.view(*new_x_shape)
-        return x.permute(0, 2, 1, 3)
-
-    def forward(self, input_tensor, attention_mask):
-        mixed_query_layer = self.query(input_tensor)
-        mixed_key_layer = self.key(input_tensor)
-        mixed_value_layer = self.value(input_tensor)
-
-        query_layer = self.transpose_for_scores(mixed_query_layer)
-        key_layer = self.transpose_for_scores(mixed_key_layer)
-        value_layer = self.transpose_for_scores(mixed_value_layer)
-
-        # Take the dot product between "query" and "key" to get the raw attention scores.
-        attention_scores = torch.matmul(query_layer, key_layer.transpose(-1, -2))
-
-        attention_scores = attention_scores / math.sqrt(self.attention_head_size)
-        attention_scores = attention_scores + attention_mask
-
-        # Normalize the attention scores to probabilities.
-        attention_probs = nn.Softmax(dim=-1)(attention_scores)
-        attention_probs = self.attn_dropout(attention_probs)
-        context_layer = torch.matmul(attention_probs, value_layer)
-        context_layer = context_layer.permute(0, 2, 1, 3).contiguous()
-        new_context_layer_shape = context_layer.size()[:-2] + (self.all_head_size,)
-        context_layer = context_layer.view(*new_context_layer_shape)
-        hidden_states = self.dense(context_layer)
-        hidden_states = self.out_dropout(hidden_states)
-        hidden_states = self.LayerNorm(hidden_states + input_tensor)
-
-        return hidden_states
-
-
-class Intermediate(nn.Module):
-    def __init__(self, args):
-        super(Intermediate, self).__init__()
-        self.dense_1 = nn.Linear(args.hidden_size, args.hidden_size * 4)
-        if isinstance(args.hidden_act, str):
-            self.intermediate_act_fn = ACT2FN[args.hidden_act]
-        else:
-            self.intermediate_act_fn = args.hidden_act
-
-        self.dense_2 = nn.Linear(args.hidden_size * 4, args.hidden_size)
-        self.LayerNorm = LayerNorm(args.hidden_size, eps=1e-12)
-        self.dropout = nn.Dropout(args.hidden_dropout_prob)
-
-    def forward(self, input_tensor):
-        hidden_states = self.dense_1(input_tensor)
-        hidden_states = self.intermediate_act_fn(hidden_states)
-
-        hidden_states = self.dense_2(hidden_states)
-        hidden_states = self.dropout(hidden_states)
-        hidden_states = self.LayerNorm(hidden_states + input_tensor)
-
-        return hidden_states
-
-
-class Layer(nn.Module):
-    def __init__(self, args):
-        super(Layer, self).__init__()
-        self.attention = SelfAttention(args)
-        self.intermediate = Intermediate(args)
-
-    def forward(self, hidden_states, attention_mask):
-        attention_output = self.attention(hidden_states, attention_mask)
-        intermediate_output = self.intermediate(attention_output)
-        return intermediate_output
-
-
-class Encoder(nn.Module):
-    def __init__(self, args):
-        super(Encoder, self).__init__()
-        layer = Layer(args)
-        self.layer = nn.ModuleList([copy.deepcopy(layer) for _ in range(args.num_hidden_layers)])
-
-    def forward(self, hidden_states, attention_mask, output_all_encoded_layers=True):
-        all_encoder_layers = []
-        for layer_module in self.layer:
-            hidden_states = layer_module(hidden_states, attention_mask)
-            if output_all_encoded_layers:
-                all_encoder_layers.append(hidden_states)
-        if not output_all_encoded_layers:
-            all_encoder_layers.append(hidden_states)
-        return all_encoder_layers
+    @staticmethod
+    def add_to_argparse(parser):
+        parser.add_argument('--max_len',          type=int,   default=50)
+        parser.add_argument('--neg_sample_size',  type=int,   default=100)
+        parser.add_argument('--batch_size',       type=int,   default=256)
+        parser.add_argument('--num_workers',      type=int,   default=4)
+        parser.add_argument('--item_size',        type=int,   default=0)
+        parser.add_argument('--interaction_path', type=str,   default=INTERACTION_PATH)
+        return parser
